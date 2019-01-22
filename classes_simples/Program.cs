c9f5f1bec8ebad7a686451a2302d5341b1f7af25
shaego <babsbouilletgmail.com>
2019-01-22 15:01:35 -0500

    Ajoutez des fichiers projet.

---
+++ classes_simples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classes_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
